d5a7e417e89c0c317128bfafe459d8da9c900fad
Cynthia Parchoux Borot <cynthia.borot@ecomail.fr>
2025-04-15 10:49:06 +0200

    add event-binding-1/

---
+++ event-binding-1/src/app/formulaire/formulaire.component.ts
@@ -0,0 +1,18 @@
+import { Component, Output, EventEmitter } from '@angular/core';
+
+@Component({
+  selector: 'app-formulaire',
+  standalone: false,
+  templateUrl: './formulaire.component.html',
+  styleUrl: './formulaire.component.scss'
+})
+export class FormulaireComponent {
+  // Use in components with the @Output directive to emit custom events 
+  // synchronously or asynchronously, and register handlers for those 
+  // events by subscribing to an instance.
+  @Output() nouvelleTacheEvt = new EventEmitter<string>();
+
+  ajoutNouvelleTache(tache: string) {
+      this.nouvelleTacheEvt.emit(tache)
+  } 
+}
